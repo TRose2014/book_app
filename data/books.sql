f6f5056cdb3fc3a8060858fbe590b85bf8baf463
Tia Rose <shortyshort789@aol.com>
2019-05-08 08:40:33 -0700

    corrected scaffolded working

--- data/books.sql
+++ data/books.sql
@@ -4,7 +4,7 @@ CREATE TABLE books_app;(
   id SERIAL PRIMARY KEY,
   author VARCHAR(255),
   title VARCHAR(255),
-  ISBN TINYINT,
+  ISBN VARCHAR(255),
   image_url TEXT,
   description TEXT,
   bookshelf VARCHAR(255)
