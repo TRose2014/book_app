9359200d33ca58d40df608d873f86cc9a8496fe2
Matt Wilkin <mwilkin22@gmail.com>
2019-05-12 00:57:58 -0700

    hidden form is showing correctly on button click.

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,10 @@ $('.viewButton').on('click', function(event){
 
   let className = $(event.target).siblings('form').attr('class');
 
-  if(event.target.className.includes('view-book')){
-    if(className.includes('hide-me')){
-      $(event.target).siblings('form').removeClass('hide-me');
-    }else{
-      $(event.target).siblings('form').addClass('hide-me');
-    }
+  if(className.includes('hide-me')){
+    $(event.target).siblings('form').removeClass('hide-me');
+  }else{
+    $(event.target).siblings('form').addClass('hide-me');
   }
 });
 
