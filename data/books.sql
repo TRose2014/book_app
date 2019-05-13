31eb8836b49e52486c1bd35e0f22c147323969ee
Tia Rose <shortyshort789@aol.com>
2019-05-12 20:45:06 -0700

    done with final stylings. updated read me. Did stretch goals for stylings. Functionality still works

--- data/books.sql
+++ data/books.sql
@@ -9,7 +9,3 @@ CREATE TABLE books (
   description TEXT,
   bookshelf VARCHAR(255)
 );
-
-INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES ('This River', 'James Brown', '9781582438740', 'http://books.google.com/books/content?id=gp1aW4T-FsgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'Description: I work.', 'Bookshelf: Fiction');
-
-INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES ('Mr. Clemens and Mark Twain', 'Justin Kaplan', '9781439129319', 'http://books.google.com/books/content?id=lj1Rc68iZ0YC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'Description: Floating down a river.', 'Bookshelf: Fiction');
